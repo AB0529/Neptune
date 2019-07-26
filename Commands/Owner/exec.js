e131cf1f3a83135d2f4731878b519882338a6ebb
MoistSenpai <supercrab115@gmail.com>
2019-07-26 00:13:41 -0400

    Cleaned up exec and utils

--- Commands/Owner/exec.js
+++ Commands/Owner/exec.js
@@ -6,13 +6,13 @@ class Ping extends Command {
 	constructor(nep) {
 		super(nep, {
 			name: path.basename(__filename, '.js'),
-			help: `Usual 'ping' command.`,
-			longHelp: `Returns the round trip ping and API latency`,
-			usage: [`• ${nep.prefix}${path.basename(__filename, '.js')}`],
-			examples: [`• ${nep.prefix}${path.basename(__filename, '.js')}`],
+			help: `Exec commands.`,
+			longHelp: `Returns commands to console.`,
+			usage: [`• ${nep.prefix}${path.basename(__filename, '.js')} <args>`],
+			examples: [`• ${nep.prefix}${path.basename(__filename, '.js')} node -v`],
 			category: path.dirname(__filename).split(path.sep).pop(),
 			cooldown: 1e3,
-			aliases: ['pong'],
+			aliases: [],
 			locked: false,
 			allowDM: true
 		});
@@ -21,67 +21,27 @@ class Ping extends Command {
 	// ---------------------------------------------------------------------------
 
 	async run(msg, util, args, nep) {
-		args = args.join(' ');
+		let m = await msg.channel.send(`*Running...*`);
 
-		return msg.channel.send({
+		try { // Run the command
+			var run = require('child_process').execSync(args.join(' '));
+		} catch (err) {
+			var run = err;
+		};
+
+		// Get the emoji for the correct response
+		let getEmote = () => { run.toString().indexOf('Error') >= 0 ? ':x:' : '✅'; }
+
+		if (run.toString().length >= 1e3) { // If output is too long send paginton
+			m.edit(`Too lazy I'll add later \`7/27/19 12:11 AM\``);
+		}
+		// If not, send the output
+		m.edit({
 			embed: new nep.discord.MessageEmbed()
-				.setDescription(`*Running...*`)
-				.setColor(nep.rColor)
-		}).then((m1) => {
-			try {
-				var run = require('child_process').execSync(args);
+				.addField(`Input`, `\`\`\`css\n${args.join(' ')}\n\`\`\``)
+				.addField(`${getEmote()} Output`, `\`\`\`css\n${run.toString()}\n\`\`\``)
+		});
 
-				if (!args[0]) return msg.channel.send(`No args boi`);
-			} catch (err) {
-				run = err;
-			}
-			let getEmote = () => {
-				if (run.toString().indexOf('Error') >= 0) return ':x:';
-				else return '✅';
-			}
-			if (run.toString().length >= 1024) {
-				let page1 = new nep.discord.MessageEmbed()
-					.addField(`Input`, `\`\`\`js\n${args}\n\`\`\``)
-					.addField(`${getEmote()} Output`, `\`\`\`js\n${run.toString().slice(0, Math.floor(run.toString().length / 2))}\n\`\`\``)
-					.setColor(nep.rColor)
-				let page2 = new nep.discord.MessageEmbed()
-					.addField(`Input`, `\`\`\`js\n${args}\n\`\`\``)
-					.addField(`${getEmote()} Output`, `\`\`\`js\n${run.toString().slice(Math.floor(run.toString().length / 2), run.toString().length)}\n\`\`\``)
-					.setColor(nep.rColor)
-				m1.edit({
-					embed: page1
-				}).then(async (mm) => {
-					const collector = mm.createReactionCollector((abc) => abc.users.last().id == msg.author.id, {
-						time: 30000
-					});
-
-					await mm.react('◀');
-					await mm.react('▶');
-
-					collector.on('collect', (m) => {
-						if (m.emoji.name == '▶') return mm.edit({
-							embed: page2
-						}).then(async () => {
-							await mm.reactions.removeAll();
-							await mm.react('◀');
-							await mm.react('▶');
-						});
-						if (m.emoji.name == '◀') return mm.edit({
-							embed: page1
-						}).then(async () => {
-							await mm.reactions.removeAll();
-							await mm.react('◀');
-							await mm.react('▶');
-						});
-					});
-				});
-			} else return m1.edit({
-				embed: new nep.discord.MessageEmbed()
-					.addField(`Input`, `\`\`\`js\n${args}\n\`\`\``)
-					.addField(`${getEmote()} Output`, `\`\`\`js\n${run}\n\`\`\``)
-					.setColor(nep.rColor)
-			});
-		}).catch((e) => nep.func.log(`Error`, e));
 	}
 }
 
--- Events/message.js
+++ Events/message.js
@@ -35,23 +35,29 @@ module.exports = class {
 		if (!isOwner) // Make sure owner lock works
 			return nep.util.embed(`:x: | You're **not my master**, go away! Shoo, shoo!`);
 		else if (cmd.cooldown.has(msg.author.id)) { // Handle command cooldown
-			if (cmd.sentCooldownMessage.has(msg.author.id)) return;
-			else return msg.channel.send({
-				embed: new nep.discord.MessageEmbed()
-					.setDescription(`⏲ | *Please wait* \`${nep.util.msParser(cmd.config.cooldown)}\` *until using this command again!*`)
-					.setColor(nep.rColor)
-					.setFooter(msg.author.tag, msg.author.displayAvatarURL())
-			}).then(() => cmd.sentCooldownMessage.add(msg.author.id));
+			if (cmd.sentCooldownMessage.has(msg.author.id))
+				return;
+			else
+				return msg.channel.send({
+					embed: new nep.discord.MessageEmbed()
+						.setDescription(`⏲ | *Please wait* \`${nep.util.msParser(cmd.config.cooldown)}\` *until using this command again!*`)
+						.setColor(nep.rColor)
+						.setFooter(msg.author.tag, msg.author.displayAvatarURL())
+				}).then(() => cmd.sentCooldownMessage.add(msg.author.id));
 		}
 
 		// Command handler
 		try {
 			cmd.setMessage(msg);
 
-			if (!cmd.config.allowDM && !msg.guild) return nep.util.embed(`:x: | This command cannot be used in a DM!`);
-			if (cmd.config.cooldown > 0) cmd.startCooldown(msg.author.id);
-			if (cmd.config.locked && msg.author.id !== `184157133187710977` && msg.author.id !== `251091302303662080`) return nep.util.embed(`🔒 | \`${command}\` has been **locked to the public**! Try again later!`);
-			if (cmd.info.category == 'Moisty' && msg.author.id !== `184157133187710977` && msg.author.id !== `251091302303662080`) return msg.channel.send(`Fuck off`);
+			if (!cmd.config.allowDM && !msg.guild)
+				return nep.util.embed(`:x: | This command cannot be used in a DM!`);
+			if (cmd.config.cooldown > 0)
+				cmd.startCooldown(msg.author.id);
+			if (cmd.config.locked && msg.author.id !== nep.config.owner)
+				return nep.util.embed(`🔒 | \`${command}\` has been **locked to the public**! Try again later!`);
+			if (cmd.info.category == 'Owner' && msg.author.id !== `184157133187710977`)
+				return msg.channel.send(`Fuck off`);
 			cmd.run(msg, nep.util, args, nep);
 		} catch (err) {
 			nep.util.error(`${err.stack}`);
