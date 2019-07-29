d27fb50de20cb02e0d2ff893330f12297af33482
MoistSenpai <supercrab115@gmail.com>
2019-07-29 02:16:48 -0400

    Test

---
+++ Commands/Owner/test.js
@@ -0,0 +1,38 @@
+const Command = require('../../Classes/Command.js');
+const stripIndents = require('common-tags').stripIndents;
+const path = require('path');
+const curl = require('curl');
+
+class Test extends Command {
+	constructor(nep) {
+		super(nep, {
+			name: path.basename(__filename, '.js'),
+			help: `Test code.`,
+			longHelp: `Test.`,
+			usage: [`• ${nep.prefix}${path.basename(__filename, '.js')} <args>`],
+			examples: [`• ${nep.prefix}${path.basename(__filename, '.js')} gdfngdk`],
+			category: path.dirname(__filename).split(path.sep).pop(),
+			cooldown: 1e3,
+			aliases: [],
+			locked: false,
+			allowDM: true
+		});
+	}
+
+	// ---------------------------------------------------------------------------
+
+	async run(msg, util, args, nep) {
+		let q = util.getQueue(msg.guild.id);
+		let row = nep.server;
+
+		q.push(`Small pp`);
+		util.updateQueue();
+		util.send(JSON.stringify(q));
+		util.send(JSON.stringify(row[0]));
+		util.send(JSON.stringify(nep.queues[msg.guild.id]));
+
+		// util.send(JSON.stringify(q));
+	}
+}
+
+module.exports = Test;
